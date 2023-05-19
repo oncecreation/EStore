c4da1b6ed91e24937e58d468fced1bbacda7c9bd
James Hussey <jameshussey474@gmail.com>
2023-05-20 05:49:23 +0900

    dev: lift-off

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,18 +1,19 @@
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+// const FacebookStrategy = require("passport-facebook").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
 
-const keys = require('./keys');
-const { EMAIL_PROVIDER } = require('../constants');
+const keys = require("./keys");
+const { EMAIL_PROVIDER } = require("../constants");
 
-const { google, facebook } = keys;
+// const { google, facebook } = keys;
+const { google, jwt } = keys;
 const { clientURL, apiURL } = keys.app;
 
-const User = mongoose.model('User');
-const secret = keys.jwt.secret;
+const User = mongoose.model("User");
+const secret = jwt.secret;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -21,24 +22,24 @@ opts.secretOrKey = secret;
 passport.use(
   new JwtStrategy(opts, (payload, done) => {
     User.findById(payload.id)
-      .then(user => {
+      .then((user) => {
         if (user) {
           return done(null, user);
         }
 
         return done(null, false);
       })
-      .catch(err => {
+      .catch((err) => {
         return done(err, false);
       });
   })
 );
 
-module.exports = async app => {
+module.exports = async (app) => {
   app.use(passport.initialize());
 
   await googleAuth();
-  await facebookAuth();
+  // await facebookAuth();
 };
 
 const googleAuth = async () => {
@@ -48,16 +49,16 @@ const googleAuth = async () => {
         {
           clientID: google.clientID,
           clientSecret: google.clientSecret,
-          callbackURL: `${clientURL}/${apiURL}/${google.callbackURL}`
+          callbackURL: `${clientURL}/${apiURL}/${google.callbackURL}`,
         },
         (accessToken, refreshToken, profile, done) => {
           User.findOne({ email: profile.email })
-            .then(user => {
+            .then((user) => {
               if (user) {
                 return done(null, user);
               }
 
-              const name = profile.displayName.split(' ');
+              const name = profile.displayName.split(" ");
 
               const newUser = new User({
                 provider: EMAIL_PROVIDER.Google,
@@ -66,7 +67,7 @@ const googleAuth = async () => {
                 firstName: name[0],
                 lastName: name[1],
                 avatar: profile.picture,
-                password: null
+                password: null,
               });
 
               newUser.save((err, user) => {
@@ -77,65 +78,65 @@ const googleAuth = async () => {
                 return done(null, user);
               });
             })
-            .catch(err => {
+            .catch((err) => {
               return done(err, false);
             });
         }
       )
     );
   } catch (error) {
-    console.log('Missing google keys');
+    console.log("Missing google keys");
   }
 };
 
-const facebookAuth = async () => {
-  try {
-    passport.use(
-      new FacebookStrategy(
-        {
-          clientID: facebook.clientID,
-          clientSecret: facebook.clientSecret,
-          callbackURL: `${clientURL}/${apiURL}/${facebook.callbackURL}`,
-          profileFields: [
-            'id',
-            'displayName',
-            'name',
-            'emails',
-            'picture.type(large)'
-          ]
-        },
-        (accessToken, refreshToken, profile, done) => {
-          User.findOne({ facebookId: profile.id })
-            .then(user => {
-              if (user) {
-                return done(null, user);
-              }
+// const facebookAuth = async () => {
+//   try {
+//     passport.use(
+//       new FacebookStrategy(
+//         {
+//           clientID: facebook.clientID,
+//           clientSecret: facebook.clientSecret,
+//           callbackURL: `${clientURL}/${apiURL}/${facebook.callbackURL}`,
+//           profileFields: [
+//             "id",
+//             "displayName",
+//             "name",
+//             "emails",
+//             "picture.type(large)",
+//           ],
+//         },
+//         (accessToken, refreshToken, profile, done) => {
+//           User.findOne({ facebookId: profile.id })
+//             .then((user) => {
+//               if (user) {
+//                 return done(null, user);
+//               }
 
-              const newUser = new User({
-                provider: EMAIL_PROVIDER.Facebook,
-                facebookId: profile.id,
-                email: profile.emails ? profile.emails[0].value : null,
-                firstName: profile.name.givenName,
-                lastName: profile.name.familyName,
-                avatar: profile.photos[0].value,
-                password: null
-              });
+//               const newUser = new User({
+//                 provider: EMAIL_PROVIDER.Facebook,
+//                 facebookId: profile.id,
+//                 email: profile.emails ? profile.emails[0].value : null,
+//                 firstName: profile.name.givenName,
+//                 lastName: profile.name.familyName,
+//                 avatar: profile.photos[0].value,
+//                 password: null,
+//               });
 
-              newUser.save((err, user) => {
-                if (err) {
-                  return done(err, false);
-                }
+//               newUser.save((err, user) => {
+//                 if (err) {
+//                   return done(err, false);
+//                 }
 
-                return done(null, user);
-              });
-            })
-            .catch(err => {
-              return done(err, false);
-            });
-        }
-      )
-    );
-  } catch (error) {
-    console.log('Missing facebook keys');
-  }
-};
+//                 return done(null, user);
+//               });
+//             })
+//             .catch((err) => {
+//               return done(err, false);
+//             });
+//         }
+//       )
+//     );
+//   } catch (error) {
+//     console.log("Missing facebook keys");
+//   }
+// };
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const chalk = require('chalk');
-const compression = require('compression');
-const cors = require('cors');
-const path = require('path');
-const helmet = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const chalk = require("chalk");
+const compression = require("compression");
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet");
 
-const keys = require('./config/keys');
-const routes = require('./routes');
-const socket = require('./socket');
-const setupDB = require('./utils/db');
+const keys = require("./config/keys");
+const routes = require("./routes");
+const socket = require("./socket");
+const setupDB = require("./utils/db");
 
 const { port } = keys;
 const app = express();
@@ -19,27 +19,27 @@ app.use(express.json());
 app.use(
   helmet({
     contentSecurityPolicy: false,
-    frameguard: true
+    frameguard: true,
   })
 );
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 setupDB();
-require('./config/passport')(app);
+require("./config/passport")(app);
 app.use(routes);
 
-console.log('process.env.NODE_ENV ', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
+console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
   app.use(compression());
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
   });
 }
 
 const server = app.listen(port, () => {
   console.log(
-    `${chalk.green('✓')} ${chalk.blue(
+    `${chalk.green("✓")} ${chalk.blue(
       `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
     )}`
   );
